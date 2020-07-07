26bd1cb483634dc51237dfa9003e838f1d335d0b
Mohamed <sawdudeenm@gmail.com>
2020-07-07 19:53:55 +0530

    initial file setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Student Management System</h1>
+         );
+    }
+}
+ 
+export default App;
