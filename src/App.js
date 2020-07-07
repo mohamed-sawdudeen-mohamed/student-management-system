f7cffbdf8ef191d0a1d4c3a10b20c284738ab426
Mohamed <sawdudeenm@gmail.com>
2020-07-07 20:30:37 +0530

    page layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ class App extends React.Component {
     state = {  }
     render() { 
         return ( 
-            <h1>Student Management System</h1>
+            <div className='container-fluid bg-light main-container'>
+                <h2 className='text-center py-3 text-primary'>Student Management System</h2>
+                <div className='row'>
+                    <div className='col-md-4'>Form</div>
+                    <div className='col-md-8'>Table</div>
+
+                </div>
+
+            </div>
          );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 import './styles/bootstrap.css';
+import './styles/styles.css';
 
 
 ReactDom.render(<App />, document.getElementById('root'));
